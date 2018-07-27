4f674472f62d79c9f7bbf0667ca2a6453922a0f4
fabian <saonagroup@gmail.com>
2018-07-26 20:21:35 -0500

    nuevaapp actual

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+/*mi codigo*/
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Alerta!',
+      subTitle: 'Acceso restringido!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Recuperar contraseña',
+      message: "Entre su correo",
+      inputs: [
+        {
+          name: 'E-mail:',
+          placeholder: 'E-mail'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Enviar correo',
+          handler: data => {
+            console.log('Saved clicked');
+            this.showAlert1(data);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showAlert1(data) {
+    let alert = this.alertCtrl.create({
+      title: 'Correo enviado!',
+      subTitle: 'Se envio un mensaje a: ' + data['E-mail:'],
+      buttons: ['OK']
+    });
+    console.log(data);
+    alert.present();
+  }
 }
